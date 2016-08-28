de0b9ae44339d636399d01a292006bf311cd02df
YAO WU <PandaGongfu@YAOs-MacBook-Pro.local>
2016-08-27 22:57:20 -0400

    completed code

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,24 @@
 # Hint:  use Google to find python function
+import datetime as dt
 
 ####a) 
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'
+start = dt.datetime.strptime(date_start, '%m-%d-%Y')
+end = dt.datetime.strptime(date_stop, '%m-%d-%Y')
+print((end - start).days)
 
 ####b)  
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015'
+start = dt.datetime.strptime(date_start, '%m%d%Y')
+end = dt.datetime.strptime(date_stop, '%m%d%Y')
+print((end - start).days)
 
 ####c)  
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'
+start = dt.datetime.strptime(date_start, '%d-%b-%Y')
+end = dt.datetime.strptime(date_stop, '%d-%b-%Y')
+print((end - start).days)
+
